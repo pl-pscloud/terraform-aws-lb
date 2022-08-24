f68054cb73956d9b07d86326779fd24861afb2e0
pswitala <p.switala@pslab.eu>
2022-08-24 10:48:12 +0200

    Extended output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "pscloud_lb_dns_name" {
-  value       = var pscloud_lb_type = "application" ? aws_lb.pscloud-alb.dns_name ? aws_lb.pscloud-nlb.dns_name
+  value       = var.pscloud_lb_type == "application" ? aws_lb.pscloud-alb.dns_name ? aws_lb.pscloud-nlb.dns_name
 }
 
 output "pscloud_lb_zone_id" {
-  value       = var pscloud_lb_type = "application" ? aws_lb.pscloud-alb.zone_id ? aws_lb.pscloud-nlb.zone_id
+  value       = var.pscloud_lb_type == "application" ? aws_lb.pscloud-alb.zone_id ? aws_lb.pscloud-nlb.zone_id
 }
 
 output "pscloud_lb_tg_arns" {
@@ -15,5 +15,5 @@ output "pscloud_lb_lsiteners" {
 }
 
 output "pscloud_lb_id" {
-  value       = var pscloud_lb_type = "application" ? aws_alb.pscloud-alb.id : aws_alb.pscloud-nlb.id
+  value       = var.pscloud_lb_type == "application" ? aws_alb.pscloud-alb.id : aws_alb.pscloud-nlb.id
 }
